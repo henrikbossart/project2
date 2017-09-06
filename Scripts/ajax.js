8c9d01f6ca8f553d357dfd700ffa8ea9fafe20e0
Morten Mulvik <mortenmu@stud.ntnu.no>
2017-09-06 12:06:29 +0200

    Updated loadDoc should work, needs testing Added the function loadHTML that uses jQuery and ajax to achieve the same thing as loadDoc

--- Scripts/ajax.js
+++ Scripts/ajax.js
@@ -1,10 +1,16 @@
+// loading html with javascript and ajax
 function loadDoc(filename) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if(this.readyState == 4 && this.status == 200) {
-            // load html file
+            document.getElementById("content").innerHTML = this.responseText;
         }
     };
     xhttp.open("GET", filename, true);
     xhttp.send();
+}
+
+// loading html with jquery and ajax
+function loadHTML(filename) {
+    $('#content').load(filename);
 }
