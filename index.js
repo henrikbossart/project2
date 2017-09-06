5a2831f863bcd41c15be50cf2cc5d704f70773de
Morten Mulvik <mortenmu@stud.ntnu.no>
2017-09-06 12:19:02 +0200

    fixed conflict

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
+var $ = require('jQuery');
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello World!');
 });
 
 app.listen(8081, function () {
-  console.log('Example app listening on port 8081!')
+  console.log('Magic is happening on port 8081!');
 });
